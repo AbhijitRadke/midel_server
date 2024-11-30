5083b9536d502a2bb7a0077be736ddb2b70a7ef2
Abhijit Radke <107233937+AbhijitRadke@users.noreply.github.com>
2024-11-30 17:11:51 +0530

    Update base36 added index.js

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 
+// Decode and parse the Firebase service account key from environment variable
+const serviceAccountKeyBase64 = process.env.FIREBASE_SERVICE_ACCOUNT_KEY; // Make sure this is set in Render's environment variables
+if (!serviceAccountKeyBase64) {
+    console.error('FIREBASE_SERVICE_ACCOUNT_KEY environment variable is missing.');
+    process.exit(1); // Exit if the key is not found
+}
+
+const serviceAccount = JSON.parse(Buffer.from(serviceAccountKeyBase64, 'base64').toString('utf8'));
+
 // Initialize Firebase Admin SDK
-const serviceAccount = require('./firebase-service-account.json'); // Replace with your file
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://movement-iot-default-rtdb.firebaseio.com/' // Replace with your Firebase URL
@@ -13,20 +21,17 @@ admin.initializeApp({
 const app = express();
 app.use(bodyParser.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000; // Use the PORT environment variable if available
 
-// Route to receive data from Arduino
 app.get('/', async (req, res) => {
-
-
     try {
-
         res.status(200).send({ message: 'Server is running' });
     } catch (error) {
         console.log(error.message);
         res.status(500).send({ message: error.message });
     }
 });
+// Route to receive data from Arduino
 app.post('/data', async (req, res) => {
     const sensorData = req.body;
 
@@ -44,6 +49,6 @@ app.post('/data', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on port ${PORT}`);
 });
